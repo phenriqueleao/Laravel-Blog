644874565da82e395781677602b9b8656d49e0e5
Marcio Almeida de Lima <marcio@jetimob.com>
2018-10-23 17:23:18 -0300

    passando o nav para um layout externo

--- resources/views/ver-artigo.blade.php
+++ resources/views/ver-artigo.blade.php
@@ -15,46 +15,7 @@
 </head>
 
 <body>
-    <!-- START NAV -->
-    <nav class="navbar">
-        <div class="container">
-            <div class="navbar-brand">
-                <a class="navbar-item" href="/">
-                        <img src="{{asset('images/laravel-blog-logo.png')}}" alt="Logo">
-                    </a>
-                <span class="navbar-burger burger" data-target="navbarMenu">
-                        <span></span>
-                <span></span>
-                <span></span>
-                </span>
-            </div>
-            <div id="navbarMenu" class="navbar-menu">
-                <div class="navbar-end">
-                    <a class="navbar-item is-active">
-                        Home
-                    </a>
-                    <div class="navbar-item has-dropdown is-hoverable">
-                        <a class="navbar-link">
-                            Nome usuário
-                        </a>
-                        <div class="navbar-dropdown">
-                            <a href="criar-artigo" class="navbar-item">
-                                Criar artigo
-                            </a>
-                            <hr class="navbar-divider">
-                            <div class="navbar-item">
-                                <a href="/logout">Logout</a>
-                            </div>
-                        </div>
-                    </div>
-                    <a href="/login" class="navbar-item is-active">
-                        Acesse sua conta
-                    </a>
-                </div>
-            </div>
-        </div>
-    </nav>
-    <!-- END NAV -->
+    @include('layouts.navbar')
 
     <div style="margin-top: 20rem;">
         <!-- START ARTICLE FEED -->
